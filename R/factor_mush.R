4d531c2ebc0da5e44f481ccc3b162c73df03c78a
AdrianLJones <adriaanjones@gmail.com>
2015-11-19 11:49:56 -0800

    2015/11/19 1148hrs.  Commit before git.  Everythign seems to work.

--- R/factor_mush.R
+++ R/factor_mush.R
@@ -1,3 +1,19 @@
+#' Combine Specific Levels of a Factor
+#'
+#' Often I was to combine two or more levels of a Factor into a new level that represents all of them in a new factor vector.
+#'
+#' @param factor_to_mush The factor that I want to edit.
+#' @param levels_to_mush A character vector of the names of the levels you want to combine
+#' @param mush_name The name of the newly created combined level.
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' test <- as.factor(c("a", "b", "c","c", "b", "a", "a1", "b1", "c1"))
+#' factor_mush(test, c("a", "c"), "ac")
+
+
 factor_mush <- function(factor_to_mush, levels_to_mush, mush_name) {
   factor_levels <- levels(factor_to_mush)
   factor_mush_index <- factor_levels %in% levels_to_mush
