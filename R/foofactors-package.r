7a048e8347e21e39948231d9b5ebe391856eb671
AdrianLJones <adriaanjones@gmail.com>
2015-11-19 21:46:54 -0800

    2015/11/19 2145hrs Have done an over all package comment thingy and done the document thing.

---
+++ R/foofactors-package.r
@@ -0,0 +1,10 @@
+#' foofactors: A project for stat545 2015
+#'
+#' Here I do some homework to practise producing a package
+#' I produced a function that combines factors' levels a the whim of the user.
+#' Then I spent a heck of a lot of time trying to turn it into a package, based off a package initiated by Jenny.
+#'
+#' @name foofactors
+#' @docType package
+NULL
+
